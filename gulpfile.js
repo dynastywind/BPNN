82e4f9e37ca56899c86bd609f745cab8a4e8c3ef
yi.a.li <yi.a.li@rakuten.com>
2017-02-27 15:59:47 +0900

    Add other consideration

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,18 @@ var del = require('del');
 var jade = require('gulp-jade');
 
 gulp.task('clean', () => {
-	del('./build');
+	return del('./build');
 });
 
-gulp.task('copy', ['clean'], (done) => {
-	gulp.src(['src/img/**/*'], {base: 'src'})
+gulp.task('copy', ['clean'], () => {
+	return gulp.src(['src/img/**/*','src/css/**/*','src/js/**/*'], {base: 'src'})
 		.pipe(gulp.dest('build'));
-	gulp.src(['src/css/**/*'], {base: 'src'})
-		.pipe(gulp.dest('build'));
-	gulp.src(['src/js/**/*'], {base: 'src'})
-		.pipe(gulp.dest('build'));
-	done();
 });
 
-gulp.task('jade', ['copy'], (done) => {
-	gulp.src('src/*.jade')
+gulp.task('jade', ['copy'], () => {
+	return gulp.src('src/*.jade')
 		.pipe(jade())
 		.pipe(gulp.dest('build'));
-	done();
 });
 
 gulp.task('default', ['jade']);
